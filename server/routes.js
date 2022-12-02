633dc2caca81cc8b39386424e6542450a6ccd198
Vanne Vega <manelp3214@gmail.com>
2022-12-01 21:34:35 -0600

    Terminamos seccion 11

--- server/routes.js
+++ server/routes.js
@@ -5,40 +5,45 @@ const push = require('./push');
 
 const mensajes = [
 
-  {
-    _id: 'XXX',
-    user: 'spiderman',
-    mensaje: 'Hola Mundo'
-  }
+    {
+        _id: 'XXX',
+        user: 'spiderman',
+        mensaje: 'Hola Mundo'
+    }
 
 ];
 
-
 // Get mensajes
-router.get('/', function (req, res) {
-  // res.json('Obteniendo mensajes');
-  res.json( mensajes );
+router.get('/', function(req, res) {
+    // res.json('Obteniendo mensajes');
+    res.json(mensajes);
 });
 
 
 // Post mensaje
-router.post('/', function (req, res) {
-  
-
-  const mensaje = {
-    mensaje: req.body.mensaje,
-    user: req.body.user
-  };
-
-  mensajes.push( mensaje );
-
-  // console.log(mensajes);
+router.post('/', function(req, res) {
 
 
-  res.json({
-    ok: true,
-    mensaje
-  });
+    // console.log( req.body.lat );
+    // console.log( req.body.lng );
+
+    const mensaje = {
+        mensaje: req.body.mensaje,
+        user: req.body.user,
+        lat: req.body.lat,
+        lng: req.body.lng,
+        foto: req.body.foto
+    };
+
+    mensajes.push(mensaje);
+
+    console.log(mensajes);
+
+
+    res.json({
+        ok: true,
+        mensaje
+    });
 });
 
 
@@ -46,47 +51,42 @@ router.post('/', function (req, res) {
 router.post('/subscribe', (req, res) => {
 
 
-  const suscripcion = req.body;
-
-  
-  push.addSubscription( suscripcion );
+    const suscripcion = req.body;
 
 
-  res.json('subscribe');
+    push.addSubscription(suscripcion);
+
+
+    res.json('subscribe');
 
 });
 
 // Almacenar la suscripción
 router.get('/key', (req, res) => {
 
-  const key = push.getKey();
+    const key = push.getKey();
 
 
-  res.send(key);
+    res.send(key);
 
 });
 
 
-// Envar una notificación PUSH a las personas
-// que nosotros queramos
+// Enviar una notificación PUSH a las personas que nosotros queramos
 // ES ALGO que se controla del lado del server
 router.post('/push', (req, res) => {
 
-  const post = {
-    titulo: req.body.titulo,
-    cuerpo: req.body.cuerpo,
-    usuario: req.body.usuario
-  };
+    const post = {
+        titulo: req.body.titulo,
+        cuerpo: req.body.cuerpo,
+        usuario: req.body.usuario
+    };
 
 
-  push.sendPush( post );
+    push.sendPush(post);
 
-  res.json( post );
+    res.json(post);
 
 });
-
-
-
-
 
 module.exports = router;
